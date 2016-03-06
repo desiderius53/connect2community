959cd6acda74c52c44f96f5bce18153732fd9879
phyushin <phyushin@gmail.com>
2016-03-06 15:05:07 +0000

    added facility to add multiple google calendars
    D
    D
    C
    C
    
    A
    B
    B

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-function getAddressesFromCalendar (map) {
-    $.ajax({
-        url: "https://clients6.google.com/calendar/v3/calendars/wigandementia@gmail.com/events?calendarId=wigandementia%40gmail.com&singleEvents=true&timeZone=Europe%2FLondon&maxAttendees=1&maxResults=250&sanitizeHtml=true&timeMin=2016-03-05T00%3A00%3A00Z&timeMax=2016-04-15T00%3A00%3A00Z&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs",
-        success: function (data) {
-            $.each(data.items, function( key, value ) {
-                console.log(value.location);
-                putLocationOnMap(value.location, map);
-            });
-        }
-    })
+function getAddressesFromCalendar (map,calendars) {
+    $.each(calendars, function(key,value) {
+        $.ajax({
+            url: value,
+            success: function (data) {
+                $.each(data.items, function( key, value ) {
+                    console.log(value.location);
+                    putLocationOnMap(value.location, map);
+                });
+            }
+        });
+    });
+}
+
+function addNewCalendar(calendars,calendar){
+    calendars.push(calendar);
+    console.log(calendar.name + "and colour :" + calendar.colour);
 }
 
 function putLocationOnMap (address, map) {
