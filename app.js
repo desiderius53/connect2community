e9251e2201b54d4303be92bb8d7243081c7e7f1b
phyushin <phyushin@gmail.com>
2016-03-09 21:31:50 +0000

    work with custom pins

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-function getAddressesFromCalendar (map,calendars) {
+// var mark = ['A','B','C','D','E','F','G''H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']; //this is a horrible way to do this
 
+ function getAddressesFromCalendar (map,calendars) {
     $.each(calendars, function(key,value) {
         $.ajax({
             url: value.url,
             success: function (data) {
+                var i =0;
                 $.each(data.items, function( key, value ) {
                     console.log(value.location);
-                    putLocationOnMap(value.location, map);
+                    putLocationOnMap(value.location, map,mark[i],'green');
+                    i++;
                 });
             }
         });
@@ -18,7 +21,7 @@ function addNewCalendar(calendars,calendar){
     console.log(calendar.name + " and colour :" + calendar.colour);
 }
 
-function putLocationOnMap (address, map) {
+function putLocationOnMap (address, map, mark, colour) {
     var geocoder = new google.maps.Geocoder();
     geocoder.geocode( {'address': address}, function(results, status) {
             console.log("looking for location "+address);
@@ -26,8 +29,9 @@ function putLocationOnMap (address, map) {
             var marker1 = new google.maps.Marker({
                 map: map,
                 position: results[0].geometry.location,
-                icon: 'assets/Google_Maps_Markers/green_MarkerA.png'
+                icon: 'assets/Google_Maps_Markers/'+ colour +'_Marker' + mark + '.png'
             });
+                console.log('assets/Google_Maps_Markers/'+ colour +'_Marker' + mark + '.png');
             console.log(results[0].geometry.location);
     });
 }
